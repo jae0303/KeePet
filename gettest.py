fb43a7a685a3b4c41cd8e61d65d8fd21b5685d63
chojaehoon <jae0303@ajou.ac.kr>
2017-11-02 20:55:44 +0900

    PI get/put data AWS

---
+++ gettest.py
@@ -0,0 +1,38 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
+
+table = dynamodb.Table('IoTButtonTable')
+
+title = "The Big New Movie"
+year = "2015"
+
+try:
+    response = table.get_item(
+        Key={
+            'SerialNumber': year,
+            'ClickType': title
+        }
+    )
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    item = response['Item']
+    print("GetItem succeeded:")
+    print(json.dumps(item, indent=4, cls=DecimalEncoder))
+
