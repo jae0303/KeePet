83100c5041c562218881f9de7694d47b8b35378a
jae0303 <30518413+jae0303@users.noreply.github.com>
2017-12-02 20:34:14 +0900

    Get/Send information using AWS
    Raspberry pi python code.
    gettest.py - Pi check dynamodb and get own's information, send info to arduino
    puttest.py - Pi check arduino's status and send information to dynamodb

--- gettest.py
+++ gettest.py
@@ -2,6 +2,9 @@ from __future__ import print_function # Python 2/3 compatibility
 import boto3
 import json
 import decimal
+import serial
+import string
+import time
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 
@@ -17,22 +20,66 @@ class DecimalEncoder(json.JSONEncoder):
 
 dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
 
-table = dynamodb.Table('IoTButtonTable')
-
-title = "The Big New Movie"
-year = "2015"
-
+table = dynamodb.Table('Control')
+count=0
+CarNum = "5"
+test=serial.Serial("/dev/ttyUSB0",9600)
+#test.open()
 try:
-    response = table.get_item(
-        Key={
-            'SerialNumber': year,
-            'ClickType': title
-        }
-    )
+    while True:
+    	response = table.get_item(
+        	Key={
+            	    'CarNum': CarNum
+        	}
+    	)
+	item = response['Item']
+	#print("GetItem success")
+	value_x = json.dumps(item["x"], indent=4, cls=DecimalEncoder)
+	value_y = json.dumps(item["y"], indent=4, cls=DecimalEncoder)
+	value_cmd = json.dumps(item["cmd"], indent=4, cls=DecimalEncoder)
+	
+	value = value_x.replace('"',"")+"/"+value_y.replace('"',"")+"/"+value_cmd.replace('"',"")
+	#value = "%d/%d/%d"%(value_x,value_y,value_cmd)
+	if value=="0/0/0" and count==0:
+		count=1
+		print(value)
+		test.write(value)
+		#time.sleep(0.5)
+	elif value=="0/0/0" and count==1:
+		print("ignore")
+		#test.write(value)
+	else:
+		print(value)
+		test.write(value)
+		#time.sleep(0.5)
+		count=0
+	time.sleep(0.5)
 except ClientError as e:
     print(e.response['Error']['Message'])
-else:
-    item = response['Item']
-    print("GetItem succeeded:")
-    print(json.dumps(item, indent=4, cls=DecimalEncoder))
+#else:
+    #item = response['Item']
+    #print("GetItem succeeded:")
+    #value_x = json.dumps(item["x"], indent=4, cls=DecimalEncoder)
+    #value_y = json.dumps(item["y"], indent=4, cls=DecimalEncoder)
+    #value_cmd = json.dumps(item["cmd"], indent=4, cls=DecimalEncoder)
+    # value = "123"
+    # print(value)
+    #rot13 = string.maketrans(
+#	"ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+#	"NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+    # test=serial.Serial("/dev/ttyUSB0",9600)
+    # test.open()
+ #   try:
+	#while True:
+		# line = test.readline()
+	# print(value)
+	# print(value)
+	#value = value_x+"/"+value_y+"/"+value_cmd
+	#print value
+	# test.write(value)
+	#test.write(bytes(value.encode("ascii")))
+	# timer.start()
+  #  except KeyboardInterrupt:
+#	pass # do cleanup here
 
+    # test.close()
