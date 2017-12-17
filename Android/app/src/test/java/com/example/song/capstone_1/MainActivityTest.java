1bb492e120ce51395eb36d1c3be6e1d946562fef
songgihwan <32060552+songgihwan@users.noreply.github.com>
2017-12-17 18:51:02 +0900

    Add files via upload
    안드로이드 TEST CALSS

---
+++ Android/app/src/test/java/com/example/song/capstone_1/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.song.capstone_1;
+
+import org.junit.Test;
+
+/**
+ * Created by song on 2017-12-07.
+ */
+
+public class MainActivityTest {
+
+    private MainActivity maInActivity;
+
+    @Test
+    public void setUp(){
+        maInActivity = new MainActivity();
+    }
+}
